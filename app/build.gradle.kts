e12b9d6b1a58b33addc507521ef45c5a24fbc9a3
kaleedtc <kde9090@proton.me>
2025-08-02 22:16:24 +0100

    chore: Bump version to 1.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kaleedtc.privacium"
         minSdk = 30
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.2"
+        versionCode = 6
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
