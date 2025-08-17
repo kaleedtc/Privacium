c4e035c6c30e607a2ed3f10887fb34cf539b7198
kaleedtc <kde9090@proton.me>
2025-08-17 20:45:23 +0100

    chore: Bump version to 1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kaleedtc.privacium"
         minSdk = 30
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
