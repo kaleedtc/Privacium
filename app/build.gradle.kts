6d270a8629ec8033ee7cd4e3de7a2b20766bdc31
kaleedtc <kde9090@proton.me>
2025-09-26 12:00:13 +0100

    chore: Bump version to 1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kaleedtc.privacium"
         minSdk = 30
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.8.0"
+        versionCode = 16
+        versionName = "1.8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
