c55442d1a40a6cfa484289383ee4ea63f459cb2f
kaleedtc <kde9090@proton.me>
2025-12-09 18:51:53 +0100

    chore: Bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.kaleedtc.privacium"
         minSdk = 30
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.9.1"
+        versionCode = 26
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
